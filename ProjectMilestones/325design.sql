0500682702c4d4453001c57f80f19a783f0e0b32
DrewHengehold <drew.hengehold@gmail.com>
2023-11-02 13:04:38 -0700

    This adds project milestones

---
+++ ProjectMilestones/325design.sql
@@ -0,0 +1,19 @@
+/*
+Drew Hengehold, Cosette Basto, Jacqueline Lyons, Shyna Kumar, Forrest Reid, Soran Vardanega
+CS 325 - Fall 2023
+LAST MODIFIED DATE: 02-Nov-2023
+*/
+
+/* directions:
+precede each SQL create table statement with a neat comment describing the table's purpose,
+explaining any attribute whose meaning is not immediately clear from its name, and elaborating on the
+domain of any attribute whose logical domain needs more description than is apparent from its physical
+domain
+*/
+
+/*demo:
+--This table tells us about the catalog
+create table catalog
+(
+    ...
+);
