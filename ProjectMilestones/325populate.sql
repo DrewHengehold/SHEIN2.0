1458640d6c47c5f1cbf71aea5420867dfe26e782
DrewHengehold <drew.hengehold@gmail.com>
2023-11-30 12:41:34 -0800

    Populate table User

--- ProjectMilestones/325populate.sql
+++ ProjectMilestones/325populate.sql
@@ -10,3 +10,48 @@
 
 */
 
+insert into user
+values(forrest_reid, ForestIsTheBest, 'ADMIN');
+
+insert into user
+values(drew_hengehold, DrewIsTheBest, 'ADMIN');
+
+insert into user
+values(cossette_bast, CossetteIsTheBest, 'ADMIN');
+
+insert into user
+values(shyna_kumar, ShynaIsTheBest, 'ADMIN');
+
+insert into user
+values(jacky_reid, JackyIsTheBest, 'ADMIN');
+
+insert into user
+values(soran_vardanega, SoranIsTheBest, 'ADMIN');
+
+insert into user
+values(john_doe, ZionNational, 'CUSTOMER');
+
+insert into user
+values(jane_smith, ILoveCats, 'CUSTOMER');
+
+insert into user
+values(bob_jones, ILoveDogs, 'CUSTOMER');
+
+insert into user
+values(micheal_brown, BrownBerkely, 'CUSTOMER');
+
+insert into user
+values(william_black, BlackChicago, 'CUSTOMER');
+
+insert into user
+values(lisa_jackson, JacksonNYC, 'CUSTOMER');
+
+insert into user
+values(ryan_taylor, Taylor13#, 'CUSTOMER');
+
+insert into user
+values(susan_white, WhiteTheColor, 'CUSTOMER');
+
+insert into user
+values(emily_green, GreenTheApple, 'CUSTOMER');
+
