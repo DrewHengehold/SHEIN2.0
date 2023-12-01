44ea5930ca97d951e7c7423ab19031ad63b019ec
DrewHengehold <drew.hengehold@gmail.com>
2023-12-01 14:54:49 -0800

    Working on php

--- Front_End/login.php
+++ Front_End/login.php
@@ -1,28 +1,55 @@
 <?php
 
-
-
-
 $db_conn_str = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)
                                            (HOST = cedar.humboldt.edu)
                                            (PORT = 1521))
                                        (CONNECT_DATA = (SID = STUDENT)))";
         
-$conn = oci_connect('cedar.humboldt.edu', '', $db_conn_str);
 
 // Create a database connection
-$conn = new mysqli($host, $username, $password, $database);
+$conn = oci_connect('djh133', 'Yellowstone1313#', $db_conn_str);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    echo("Connection failed");
+    exit;
 }
+
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <link rel="stylesheet" href="login.css">
+    <title>login</title>
+</head>
+
+<body style="background-image: linear-gradient(to bottom right, rgb(223, 235, 234), rgb(190, 252, 228)); background-attachment: fixed;">  
+    <div>
+        <form action="login.php" method="post" id="loginForm">
+            <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+            
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+            
+                <button class="thisbutton" type="button" id="login">Login</button>
+                
+                <button onclick = "window.location.href = 'createprofile.html';"class="thisbutton" type="button" id="create">Create Account</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
 
 if (isset($_POST['uname']) && isset($_POST['psw'])) {
     $username = $_POST['uname'];
     $password = $_POST['psw'];
 
     // Perform a query to check the credentials
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $query = "SELECT * FROM new_user WHERE username='$username' AND password='$password'";
     $result = $conn->query($query);
 
     if ($result->num_rows == 1) {
