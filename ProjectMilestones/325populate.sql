c588d5ef8dbf7167ae9ad71dfdf87089fe4f38b1
jrl127 <jrl127@humboldt.edu>
2023-12-02 11:09:11 -0800

    Update 325populate.sql

--- ProjectMilestones/325populate.sql
+++ ProjectMilestones/325populate.sql
@@ -398,50 +398,49 @@ shipment - jacky
 */
 
 insert into shipment
-values ('sh0000','111111','a123456', 'PICK', null)
+values ('sh0000','111111','a123456', 'PICK', null);
 
 insert into shipment
-values ('sh0001','222222' ,'b123456', 'PICK', null)
+values ('sh0001','222222' ,'b123456', 'PICK', null);
 
 insert into shipment
-values ('sh0002','333333','c123456', 'PICK', null)
+values ('sh0002','333333','c123456', 'PICK', null);
 
 insert into shipment
-values ('sh0003','444444','d123456', 'PICK', null)
+values ('sh0003','444444','d123456', 'PICK', null);
 
 insert into shipment
-values ('sh0004','555555','e123456', 'PACK', null)
+values ('sh0004','555555','e123456', 'PACK', null);
 
 insert into shipment
-values ('sh0005','666666' ,'f123456', 'PACK', null)
+values ('sh0005','666666' ,'f123456', 'PACK', null);
 
 insert into shipment
-values ('sh0006','777777','g123456', 'PACK', null)
+values ('sh0006','777777','g123456', 'PACK', null);
 
 insert into shipment
-values ('sh0007','888888','h123456', 'PACK', null)
+values ('sh0007','888888','h123456', 'PACK', null);
 
 insert into shipment
-values ('sh0008','999999','i123456', 'SHIP', null)
+values ('sh0008','999999','i123456', 'SHIP','02-Dec-2023');
 
 insert into shipment
-values ('sh0009','121212' ,'j123456', 'SHIP', null)
+values ('sh0009','121212' ,'j123456', 'SHIP','03-Dec-2023');
 
 insert into shipment
-values ('sh0010','131313' ,'k123456', 'SHIP', null)
+values ('sh0010','131313' ,'k123456', 'SHIP','04-Dec-2023');
 
 insert into shipment
-values ('sh0011','141414' ,'l123456', 'SHIP', null)
+values ('sh0011','141414' ,'l123456', 'SHIP','05-Dec-2023');
 
 insert into shipment
-values ('sh0012','151515','m123456', 'SHIP', null)
+values ('sh0012','151515','m123456', 'SHIP','06-Dec-2023');
 
 insert into shipment
-values ('sh0013','161616','n123456', 'SHIP', null)
+values ('sh0013','161616','n123456', 'SHIP','07-Dec-2023');
 
 insert into shipment
-values ('sh0014','171717' ,'o123456', 'SHIP', null)
-
+values ('sh0014','171717' ,'o123456', 'SHIP','08-Dec-2023');
 
 
 /*
