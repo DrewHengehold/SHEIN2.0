9c345758e6c1417e34dc464d7eb756c66d374382
jrl127 <jrl127@humboldt.edu>
2023-12-02 10:56:28 -0800

    Update 325populate.sql

--- ProjectMilestones/325populate.sql
+++ ProjectMilestones/325populate.sql
@@ -399,49 +399,49 @@ shipment - jacky
 */
 
 insert into shipment
-values ('sh0000','111111','**', 'PICK', null)
+values ('sh0000','111111','a123456', 'PICK', null)
 
 insert into shipment
-values ('sh0001','222222' ,** , 'PICK', null)
+values ('sh0001','222222' ,'b123456', 'PICK', null)
 
 insert into shipment
-values ('sh0002','333333',** , 'PICK', null)
+values ('sh0002','333333','c123456', 'PICK', null)
 
 insert into shipment
-values ('sh0003','444444', ***, 'PICK', null)
+values ('sh0003','444444','d123456', 'PICK', null)
 
 insert into shipment
-values ('sh0004','555555', **, 'PACK', null)
+values ('sh0004','555555','e123456', 'PACK', null)
 
 insert into shipment
-values ('sh0005','666666' , **, 'PACK', null)
+values ('sh0005','666666' ,'f123456', 'PACK', null)
 
 insert into shipment
-values ('sh0006','777777', **, 'PACK', null)
+values ('sh0006','777777','g123456', 'PACK', null)
 
 insert into shipment
-values ('sh0007','888888', **, 'PACK', null)
+values ('sh0007','888888','h123456', 'PACK', null)
 
 insert into shipment
-values ('sh0008','999999', **, 'SHIP', null)
+values ('sh0008','999999','i123456', 'SHIP', null)
 
 insert into shipment
-values ('sh0009','121212' ,** , 'SHIP', null)
+values ('sh0009','121212' ,'j123456', 'SHIP', null)
 
 insert into shipment
-values ('sh0010','131313' , **, 'SHIP', null)
+values ('sh0010','131313' ,'k123456', 'SHIP', null)
 
 insert into shipment
-values ('sh0011','141414' ,** , 'SHIP', null)
+values ('sh0011','141414' ,'l123456', 'SHIP', null)
 
 insert into shipment
-values ('sh0012','151515', **, 'SHIP', null)
+values ('sh0012','151515','m123456', 'SHIP', null)
 
 insert into shipment
-values ('sh0013','161616', **, 'SHIP', null)
+values ('sh0013','161616','n123456', 'SHIP', null)
 
 insert into shipment
-values ('sh0014','171717' , **, 'SHIP', null)
+values ('sh0014','171717' ,'o123456', 'SHIP', null)
 
 
 /*
