1cc9c35c6ffa3e48b12c9c2f5cf8f096892984a1
skk45 <skk45@humboldt.edu>
2023-12-02 02:17:59 -0800

    Create 325populate.sql

---
+++ ProjectMilestones/325populate.sql
@@ -0,0 +1,665 @@
+/*---
+     catalog_Items
+---*/
+
+insert into catalog_Items
+values
+('SKU001', 'Pants', 'Comfortable pants for women', 29.99, 20, 'W', 'Blue');
+
+insert into catalog_Items
+values
+('SKU002', 'Leggings', 'Stylish leggings for women', 25.99, 20, 'W', 'Black');
+
+insert into catalog_Items
+values
+('SKU003', 'Jeans', 'Casual jeans for women', 39.99, 30, 'W', 'White');
+
+insert into catalog_Items
+values
+('SKU004', 'Shorts', 'Atheletic shorts for women', 22.99, 30, 'W', 'Gray');
+
+insert into catalog_Items
+values
+('SKU005', 'High-waisted jeans', 'High-waisted jeans for women', 27.99, 30, 'W', 'Black');
+
+insert into catalog_Items
+values
+('SKU006', 'Sweatpants', 'Solid Slant pocket Sweatpants for women', 15.99, 30, 'W', 'Brown');
+
+insert into catalog_Items
+values
+('SKU007', 'Pants', 'Classic regular fit pants for men', 34.99, 30, 'M', 'Black');
+
+insert into catalog_Items
+values
+('SKU008', 'Shorts', 'Cargo shorts for men', 24.99, 30, 'M', 'Khaki');
+
+insert into catalog_Items
+values
+('SKU009', 'Joggers', 'Casual joggers for men', 29.99, 30, 'M', 'Navy');
+
+insert into catalog_Items
+values
+('SKU010', 'Cargo pants', 'Flap pocket side drawstring waist for men', 37.99, 30, 'M', 'Gray');
+
+insert into catalog_Items
+values
+('SKU011', 'Sweatpants', 'Basic slant pocket drawstring sweatpants for men', 16.99, 30, 'M', 'Red');
+
+
+insert into catalog_Items
+values
+('SKU012', 'Straight leg jeans', 'Cotton ripped straight leg jeans for men', 32.99, 30, 'M', 'Light blue');
+
+
+insert into catalog_Items
+values
+('SKU013', 'Women T-shirt', 'Causual cotton t-shirt for women', 7.99, 25, 'W', 'Pink');
+
+insert into catalog_Items
+values
+('SKU014', 'Sweater', 'Women knitted winter sweater', 25.99, 20, 'W', 'Floral');
+
+
+insert into catalog_Items
+values
+('SKU015', 'Blouse', 'Formal white blouse for women', 22.99, 20, 'W', 'White');
+
+
+insert into catalog_Items
+values
+('SKU016', 'Tank-Tops', 'Solid Tank Tops for women', 5.99, 25, 'W', 'Orange');
+
+
+insert into catalog_Items
+values
+('SKU017', 'High-Neck Sweater', 'Women turtleneck drop shoulder waffle knit sweater', 32.99, 25, 'W', 'Yellow');
+
+
+insert into catalog_Items
+values
+('SKU018', 'Men T-shirts', 'Basic casual cotton t-shirt for men', 5.99, 25, 'M', 'Red');
+
+insert into catalog_Items
+values
+('SKU019', 'Men shirt', 'Plaid print flap pocket shirt for men', 19.99, 25, 'M', 'Multicolor');
+
+
+insert into catalog_Items
+values
+('SKU020', 'Hoodie', 'Sporty hoodie for men', 22.99, 35, 'M', 'Apricot');
+
+
+insert into catalog_Items
+values
+('SKU021', 'Sweatshirt', 'Coloblock letter patched detail sweatshirt for men', 25.99, 35, 'M', 'Blue');
+
+
+insert into catalog_Items
+values
+('SKU022', 'Men zip-up sweatshirt', 'Solid zip up funnel neck sweatshirt', 27.99, 20, 'M', 'Dark grey');
+
+
+insert into catalog_Items
+values
+('SKU023', 'Sandals', 'Summer elastic slingback wedge sandals', 25.99, 20, 'W', 'White');
+
+
+insert into catalog_Items
+values
+('SKU024', 'Women sneakers', 'Casual lace-up womens sneakers', 39.99, 20, 'W', 'Red');
+
+
+insert into catalog_Items
+values
+('SKU025', 'Sports shoes', 'Women running shoes with synthetic leather', 33.99, 20, 'W', 'Multicolor');
+
+
+insert into catalog_Items
+values
+('SKU026', 'Women wedge sneakers', 'Womens platform sneakers with wedge heels', 25.99, 20, 'W', 'Beige');
+
+
+insert into catalog_Items
+values
+('SKU027', 'Men sneakers', 'Men fashionable lightweight colorblock sneakers', 39.99, 20, 'M', 'Green');
+
+
+insert into catalog_Items
+values
+('SKU028', 'Boots', 'Men high top work boots with steel toe cap', 45.99, 20, 'M', 'Black');
+
+
+insert into catalog_Items
+values
+('SKU029', 'Loafers', 'Mens fashionable and comfortable loafers', 25.99, 20, 'M', 'Bronze');
+
+
+insert into catalog_Items
+values
+('SKU030', 'Dress shoes', 'Mens business wingtip shoes', 32.99, 20, 'M', 'Brown');
+
+
+/*---
+     bottoms
+---*/
+
+insert into bottoms
+values
+('Cotton', 'SKU001' );
+
+insert into bottoms
+values
+('Spandex', 'SKU002' );
+
+insert into bottoms
+values
+('Denim', 'SKU003');
+
+
+insert into bottoms
+values
+('Polyster', 'SKU004');
+
+insert into bottoms
+values
+('Cotton', 'SKU005');
+
+
+insert into bottoms
+values
+('Polyster', 'SKU006');
+
+
+insert into bottoms
+values
+('Polyster', 'SKU007');
+
+insert into bottoms
+values
+('Spandex', 'SKU008');
+
+insert into bottoms
+values
+('Spandex', 'SKU009');
+
+insert into bottoms
+values
+('Polyster', 'SKU010');
+
+insert into bottoms
+values
+('Cotton', 'SKU011');
+
+
+insert into bottoms
+values
+('Cotton', 'SKU012');
+
+/*---
+     bottom_sizes
+---*/
+
+insert into bottom_sizes
+values
+(26, 'SKU001');
+
+insert into bottom_sizes
+values
+(27, 'SKU001');
+
+insert into bottom_sizes
+values
+(28, 'SKU001');
+
+
+insert into bottom_sizes
+values
+(26, 'SKU002');
+
+insert into bottom_sizes
+values
+(27, 'SKU002');
+
+insert into bottom_sizes
+values
+(28, 'SKU002');
+
+insert into bottom_sizes
+values
+(26, 'SKU003');
+
+insert into bottom_sizes
+values
+(27, 'SKU003');
+
+insert into bottom_sizes
+values
+(28, 'SKU003');
+
+
+insert into bottom_sizes
+values
+(26, 'SKU004');
+
+insert into bottom_sizes
+values
+(27, 'SKU004');
+
+
+insert into bottom_sizes
+values
+(28, 'SKU004');
+
+insert into bottom_sizes
+values
+(26, 'SKU005');
+
+insert into bottom_sizes
+values
+(27, 'SKU005');
+
+insert into bottom_sizes
+values
+(28, 'SKU005');
+
+
+insert into bottom_sizes
+values
+(26, 'SKU006');
+
+insert into bottom_sizes
+values
+(27, 'SKU006');
+
+insert into bottom_sizes
+values
+(28, 'SKU006');
+
+insert into bottom_sizes
+values
+(32, 'SKU007');
+
+insert into bottom_sizes
+values
+(34, 'SKU007');
+
+insert into bottom_sizes
+values
+(36, 'SKU007');
+
+insert into bottom_sizes
+values
+(32, 'SKU008');
+
+insert into bottom_sizes
+values
+(34, 'SKU008');
+
+insert into bottom_sizes
+values
+(36, 'SKU008');
+
+insert into bottom_sizes
+values
+(32, 'SKU009');
+
+insert into bottom_sizes
+values
+(34, 'SKU009');
+
+insert into bottom_sizes
+values
+(36, 'SKU009');
+
+insert into bottom_sizes
+values
+(32, 'SKU010');
+
+insert into bottom_sizes
+values
+(34, 'SKU010');
+
+insert into bottom_sizes
+values
+(36, 'SKU010');
+
+
+insert into bottom_sizes
+values
+(32, 'SKU011');
+
+insert into bottom_sizes
+values
+(34, 'SKU011');
+
+insert into bottom_sizes
+values
+(36, 'SKU011');
+
+
+insert into bottom_sizes
+values
+(32, 'SKU012');
+
+insert into bottom_sizes
+values
+(34, 'SKU012');
+
+insert into bottom_sizes
+values
+(36, 'SKU012');
+
+/*---
+     tops
+---*/
+
+insert into tops
+values
+('Short', 'SKU013', 'Round' );
+
+
+insert into tops
+values
+('Long', 'SKU014', 'Round-Neck' );
+
+insert into tops
+values
+('Long', 'SKU015', 'V-Neck' );
+
+insert into tops
+values
+('Short', 'SKU016', 'High-Neck' );
+
+insert into tops
+values
+('Long', 'SKU017', 'High-Neck' );
+
+
+insert into tops
+values
+('Short', 'SKU018', 'Round-Neck' );
+
+insert into tops
+values
+('Long', 'SKU019', 'Collar' );
+
+insert into tops
+values
+('Long', 'SKU020', 'Hooded' );
+
+insert into tops
+values
+('Long', 'SKU021', 'Crew-Neck' );
+
+insert into tops
+values
+('Long', 'SKU022', 'Round' );
+
+
+/*---
+     top_sizes
+---*/
+
+
+insert into top_sizes
+values
+('S', 'SKU013');
+
+insert into top_sizes
+values
+('M', 'SKU013');
+
+insert into top_sizes
+values
+('L', 'SKU013');
+
+insert into top_sizes
+values
+('S', 'SKU014');
+
+insert into top_sizes
+values
+('M', 'SKU014');
+
+insert into top_sizes
+values
+('L', 'SKU014');
+
+insert into top_sizes
+values
+('S', 'SKU015');
+
+insert into top_sizes
+values
+('M', 'SKU015');
+
+insert into top_sizes
+values
+('L', 'SKU015');
+
+insert into top_sizes
+values
+('S', 'SKU016');
+
+insert into top_sizes
+values
+('M', 'SKU016');
+
+insert into top_sizes
+values
+('L', 'SKU016');
+
+insert into top_sizes
+values
+('S', 'SKU017');
+
+insert into top_sizes
+values
+('M', 'SKU017');
+
+insert into top_sizes
+values
+('L', 'SKU017');
+
+insert into top_sizes
+values
+('S', 'SKU018');
+
+insert into top_sizes
+values
+('M', 'SKU018');
+
+insert into top_sizes
+values
+('L', 'SKU018');
+
+insert into top_sizes
+values
+('S', 'SKU019');
+
+insert into top_sizes
+values
+('M', 'SKU019');
+
+insert into top_sizes
+values
+('L', 'SKU019');
+
+insert into top_sizes
+values
+('S', 'SKU020');
+
+insert into top_sizes
+values
+('M', 'SKU020');
+
+insert into top_sizes
+values
+('L', 'SKU020');
+
+insert into top_sizes
+values
+('S', 'SKU021');
+
+insert into top_sizes
+values
+('M', 'SKU021');
+
+insert into top_sizes
+values
+('L', 'SKU021');
+
+insert into top_sizes
+values
+('S', 'SKU022');
+
+insert into top_sizes
+values
+('M', 'SKU022');
+
+insert into top_sizes
+values
+('L', 'SKU022');
+
+
+/*---
+     shoes
+---*/
+
+insert into shoes
+values
+('Sandals', 'SKU023');
+
+insert into shoes
+values
+('Sneaker', 'SKU024');
+
+insert into shoes
+values
+('Sports', 'SKU025');
+
+insert into shoes
+values
+('Wedge sneaker', 'SKU026');
+
+insert into shoes
+values
+('Sneaker', 'SKU027');
+
+insert into shoes
+values
+('Boots', 'SKU028');
+
+insert into shoes
+values
+('Loafers', 'SKU029');
+
+insert into shoes
+values
+('Dress shoes', 'SKU030');
+
+
+/*---
+     shoe_sizes
+---*/
+
+
+insert into shoe_sizes
+values
+(7, 'SKU023');
+
+insert into shoe_sizes
+values
+(8, 'SKU023');
+
+insert into shoe_sizes
+values
+(9, 'SKU023');
+
+insert into shoe_sizes
+values
+(7, 'SKU024');
+
+insert into shoe_sizes
+values
+(8, 'SKU024');
+
+insert into shoe_sizes
+values
+(9, 'SKU024');
+
+insert into shoe_sizes
+values
+(7, 'SKU025');
+
+insert into shoe_sizes
+values
+(8, 'SKU025');
+
+insert into shoe_sizes
+values
+(9, 'SKU025');
+
+insert into shoe_sizes
+values
+(7, 'SKU026');
+
+insert into shoe_sizes
+values
+(8, 'SKU026');
+
+insert into shoe_sizes
+values
+(9, 'SKU026');
+
+insert into shoe_sizes
+values
+(10, 'SKU027');
+
+insert into shoe_sizes
+values
+(11, 'SKU027');
+
+insert into shoe_sizes
+values
+(12, 'SKU027');
+
+insert into shoe_sizes
+values
+(10, 'SKU028');
+
+insert into shoe_sizes
+values
+(11, 'SKU028');
+
+insert into shoe_sizes
+values
+(12, 'SKU028');
+
+insert into shoe_sizes
+values
+(10, 'SKU029');
+
+insert into shoe_sizes
+values
+(11, 'SKU029');
+
+insert into shoe_sizes
+values
+(12, 'SKU029');
+
+insert into shoe_sizes
+values
+(10, 'SKU030');
+
+insert into shoe_sizes
+values
+(11, 'SKU030');
+
+insert into shoe_sizes
+values
+(12, 'SKU030');
+
+
+
