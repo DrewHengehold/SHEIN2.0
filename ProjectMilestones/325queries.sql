7adc1973936150cf412a768dc64fa830606cb3eb
cosetteBasto <cosette.basto2@gmail.com>
2023-12-05 12:53:09 -0800

    Added to files

--- ProjectMilestones/325queries.sql
+++ ProjectMilestones/325queries.sql
@@ -1,3 +1,26 @@
+/*
+Shien 2.0
+Drew Hengehold, Cosette Basto, Jacqueline Lyons, Shyna Kumar, Forrest Reid, Soran Vardanega
+CS 325 - Fall 2023
+December 5, 2023
+*/
+
 spool 325query-results.txt
+
+prompt === Query 1 ===
+
+prompt === Query 2 ===
+
+prompt === Query 3 ===
+
+prompt === Query 4 ===
+
+prompt === Query 5 ===
+
+prompt === Query 6 ===
+
+prompt === Query 7 ===
+
+prompt === Query 8 ===
 
 spool off
--- ProjectMilestones/325report2.sql
+++ ProjectMilestones/325report2.sql
@@ -1,3 +1,10 @@
+/*
+Shien 2.0
+Drew Hengehold, Cosette Basto, Jacqueline Lyons, Shyna Kumar, Forrest Reid, Soran Vardanega
+CS 325 - Fall 2023
+December 5, 2023
+*/
+
 spool report2-results.txt
 
 spool off
